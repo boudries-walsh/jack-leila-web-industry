1af28ab9c87ed2ed5c6fa1cc1bea1642fd8b1e75
jwalsh2022 <49983906+jwalsh2022@users.noreply.github.com>
2021-03-15 10:53:27 -0400

    update

--- js/topnav.js
+++ js/topnav.js
@@ -3,6 +3,6 @@ function myFunction() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
-  } else {
+  } else 
     x.className = "topnav";
   }
